c1f11aec2bcf16391b496f0f99ca9bb5ef51291d
Lihao <lih046@ucsd.edu>
2017-03-09 21:11:29 -0800

    Merged with Navigation.

--- app/src/main/java/com/example/lingfeng/dopeaf1/Login.java
+++ app/src/main/java/com/example/lingfeng/dopeaf1/Login.java
@@ -14,7 +14,6 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-import java.lang.Process;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -40,6 +39,8 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.UUID;
 
+import static android.R.string.ok;
+
 public class Login extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
     private static final String TAG = "GoogleActivity";
     private static final int RC_SIGN_IN = 9001;
@@ -49,23 +50,49 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
     private EditText email;
     private EditText password;
     private Button btnLogin;
-    private Button btnSignUp;
+    //private Button btnSignUp;
     private Button btnForgotPassword;
     private SignInButton googleSignin;
+    //private CheckBox rememberMe;
+    //private CheckBox autoLogin;
+    private SharedPreferences loginPreferences;
+    private SharedPreferences.Editor loginPrefsEditor;
+    private Boolean saveLogin;
+    private String username;
+    private String pswd;
+
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
         email = (EditText) findViewById(R.id.email);
         password = (EditText) findViewById(R.id.password);
-        btnLogin = (Button) findViewById(R.id.login);
-        btnSignUp = (Button) findViewById(R.id.register);
+        //btnLogin = (Button) findViewById(R.id.login);
+        //btnSignUp = (Button) findViewById(R.id.register);
         btnForgotPassword = (Button) findViewById(R.id.forgotPassword);
         googleSignin = (SignInButton) findViewById(R.id.sign_in_button);
+        //rememberMe = (CheckBox)findViewById(R.id.rememberme);
+        //autoLogin = (CheckBox)findViewById(R.id.autoLogin);
+        loginPreferences = getSharedPreferences("loginPrefs", MODE_PRIVATE);
+        loginPrefsEditor = loginPreferences.edit();
+/*
+        saveLogin = loginPreferences.getBoolean("saveLogin", false);
+        if (saveLogin == true) {
+            email.setText(loginPreferences.getString("username", ""));
+            password.setText(loginPreferences.getString("password", ""));
+            rememberMe.setChecked(true);
+            autoLogin.setChecked(loginPreferences.getBoolean("autoLogin", false));
+        }
+        */
+
+        googleSignin.setColorScheme(0);
+        TextView textView = (TextView) googleSignin.getChildAt(0);
+        textView.setText("Sign in with Google");
 
         if (loggedin != null) {
             email.setText(loggedin.getUserEmail());
@@ -92,6 +119,26 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
             }
         };
 
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("700971486111-difngrpa9942udtnr6s3f0vap9qiojjq.apps.googleusercontent.com")
+                .requestEmail()
+                .build();
+
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+
+
+        //TODO
+        /*
         //triggered when click on login button
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -105,53 +152,34 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
                     final String emailU = email.getText().toString();
                     final String passwordU = password.getText().toString();
 
-                    mDatabase.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
-                        int flag = 0;
+                    mAuth.signInWithEmailAndPassword(emailU, passwordU)
+                            .addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    // If sign in fails, display a message to the user. If sign in succeeds
+                                    // the auth state listener will be notified and logic to handle the
+                                    // signed in user can be handled in the listener
+                                    if (!task.isSuccessful()) {
+                                        Toast.makeText(Login.this, "Failes", Toast.LENGTH_LONG).show();
 
-                        @Override
-                        public void onDataChange(DataSnapshot dataSnapshot) {
+                                    } else {
+                                        Toast.makeText(Login.this, "Success", Toast.LENGTH_LONG).show();
 
-                            //TODO: update searching to hashmap
-                            for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-
-                                //search through each user
-                                User user = snapshot.getValue(User.class);
-
-                                //if the email and password all match
-                                if (emailU.equals(user.getUserEmail()) && passwordU.equals(user.getUserPassword())) {
-                                    loggedin = user;
-                                    Toast.makeText(Login.this, "Hello " + loggedin.getUsername(), Toast.LENGTH_SHORT).show();
-                                    flag = 1;
-                                    //define a jump
-                                    Intent intent = new Intent(Login.this, Navigation.class);
-
-                                    loggedin.updateLastlogin();
-                                    mDatabase.child("users").child(loggedin.getUserID()).setValue(loggedin);
-                                    //jump to add class
-                                    startActivity(intent);
-
-                                    revokeAccess();
-
-                                    //if the email matches but password does not match
-                                } else if (flag != 1 && emailU.equals(user.getUserEmail()) && !passwordU.equals(user.getUserPassword())) {
-                                    flag = 2;
+                                        FirebaseUser userB = task.getResult().getUser();
+                                        if(!userB.isEmailVerified()){
+                                            Toast.makeText(Login.this, userB.getEmail() + " Not Email verified", Toast.LENGTH_LONG).show();
+                                        }
+                                        else {
+                                            Toast.makeText(Login.this, userB.getEmail() + " Email verified", Toast.LENGTH_LONG).show();
+                                            loggedin = new User("New User",userB.getEmail(),userB.getUid(),"password");
+                                            //Toast.makeText(Login.this, userB.getDisplayName(), Toast.LENGTH_LONG).show();
+                                            Intent intent = new Intent(Login.this, Navigation.class);
+                                            startActivity(intent);
+                                            finish();
+                                        }
+                                    }
                                 }
-                            }
-
-                            // if user enters a wrong password but valid email
-                            if (flag == 2) {
-                                Toast.makeText(Login.this, "Wrong email or password!", Toast.LENGTH_SHORT).show();
-
-                                // if user enters new contents
-                            } else if (flag == 0) {
-                                Toast.makeText(Login.this, "User Not Exist!", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-
-                        @Override
-                        public void onCancelled(DatabaseError databaseError) {
-                        }
-                    });
+                            });
                 } else if (email.getText().length() < 1) {
                     //notify user that the email is invalid
                     Toast.makeText(Login.this, "Email address has not entered yet!", Toast.LENGTH_SHORT).show();
@@ -216,6 +244,8 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
             }
         });
 
+        */
+
         //TODO FORGOT PSWD
         //triggered when click on forgot password button
         btnForgotPassword.setOnClickListener(new View.OnClickListener() {
@@ -226,7 +256,7 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
 
                 //define a jump
                 //TODO: change the view, and may need to PASS the EMAIL user entered, so that they does not need to enter it again
-                Intent intent = new Intent(Login.this, AddClass.class);
+                Intent intent = new Intent(Login.this, Navigation.class);
                 final User userNew = new User("Forgot Password", "Forgot Password", "Forgot Password", "Forgot Password");
                 loggedin = userNew;
                 mDatabase.child("users").child(loggedin.getUserID()).setValue(loggedin);
@@ -234,26 +264,70 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
                 startActivity(intent);
             }
         });
+/*
+        rememberMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(email.getWindowToken(), 0);
 
-        // Configure sign-in to request the user's ID, email address, and basic
-        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
-        // Configure Google Sign In
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken("700971486111-difngrpa9942udtnr6s3f0vap9qiojjq.apps.googleusercontent.com")
-                .requestEmail()
-                .build();
-        // Build a GoogleApiClient with access to the Google Sign-In API and the
-        // options specified by gso.
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                .build();
+                username= email.getText().toString();
+                pswd = password.getText().toString();
+
+                if(!rememberMe.isChecked()) {
+                    autoLogin.setChecked(false);
+                }
+
+                if(rememberMe.isChecked()) {
+                    loginPrefsEditor.putBoolean("saveLogin", true);
+                    loginPrefsEditor.putString("username", username);
+                    loginPrefsEditor.putString("password", pswd);
+                    loginPrefsEditor.commit();
+                } else {
+                    loginPrefsEditor.clear();
+                    loginPrefsEditor.commit();
+                }
+                //Do something here if needed
+            }
+        });
+
+        autoLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(autoLogin.isChecked() && rememberMe.isChecked()) {
+                    loginPrefsEditor.putBoolean("autoLogin", true);
+                    loginPrefsEditor.commit();
+                } else if(autoLogin.isChecked() && !rememberMe.isChecked()) {
+                    rememberMe.performClick();
+                    loginPrefsEditor.putBoolean("autoLogin", true);
+                    loginPrefsEditor.commit();
+                }
+                else {
+                    loginPrefsEditor.putBoolean("autoLogin", false);
+                    loginPrefsEditor.commit();
+                }
+            }
+        });
+
+        */
+
+
+
+        System.out.println("Process to this step!");
 
         googleSignin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 signIn();
+
+                System.out.println("Finished signIn");
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                System.out.println("Firebase authen user");
+
+                if (user == null){
+                    System.out.println("The user is currently a null object dude");
+                }
                 if (user != null) {
                     // Name, email address, and profile photo Url
                     final String name = user.getDisplayName();
@@ -266,28 +340,12 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
                     // The user's ID, unique to the Firebase project. Do NOT use this value to
                     // authenticate with your backend server, if you have one. Use
                     // FirebaseUser.getToken() instead.
-                    String uid = user.getUid();
+                    final String uid = user.getUid();
 
                     final User userNew = new User(name, email, uid, "123456");
                     //put user into users field
                     loggedin = userNew;
 
-                    //define a jump
-                    //TODO: change the view
-                            /*if(mGoogleApiClient.isConnected()){
-                                Intent intent = new Intent(Login.this, AddClass.class);
-
-                                loggedin.updateLastlogin();
-
-                                startActivity(intent);
-                            }
-                            else {
-                                Intent intent = new Intent(Login.this, Signup.class);
-
-                                loggedin.updateLastlogin();
-
-                                startActivity(intent);
-                            }*/
                     mDatabase.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
                         int flag = 0;
 
@@ -301,10 +359,10 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
                                 User user = snapshot.getValue(User.class);
 
                                 //if the email and password all match
-                                if (email.equals(user.getUserEmail()) && name.equals(user.getUsername())) {
+                                if (uid.equals(user.getUserID())) {
                                     //define a jump
                                     Toast.makeText(Login.this, "Hello " + name, Toast.LENGTH_SHORT).show();
-                                    Intent intent = new Intent(Login.this, AddClass.class);
+                                    Intent intent = new Intent(Login.this, Navigation.class);
                                     flag = 1;
                                     loggedin.updateLastlogin();
                                     //jump to add class
@@ -340,6 +398,9 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
     public void onStart() {
         super.onStart();
         mAuth.addAuthStateListener(mAuthListener);
+//        if(autoLogin.isChecked()) {
+//            btnLogin.performClick();
+//        }
     }
 
     @Override
@@ -353,22 +414,9 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
 
 
     private void signIn() {
+        System.out.println("Entering the signIn(), line 404");
         Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
         startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-
-    private void revokeAccess() {
-        // Firebase sign out
-        mAuth.signOut();
-
-        // Google revoke access
-        Auth.GoogleSignInApi.revokeAccess(mGoogleApiClient).setResultCallback(
-                new ResultCallback<Status>() {
-                    @Override
-                    public void onResult(@NonNull Status status) {
-
-                    }
-                });
     }
 
 
@@ -390,6 +438,7 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
     }
 
     private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        System.out.println("Entering firebase Auth with google");
         Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
         AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
         mAuth.signInWithCredential(credential)
@@ -402,9 +451,13 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
                         // the auth state listener will be notified and logic to handle the
                         // signed in user can be handled in the listener.
                         if (!task.isSuccessful()) {
+                            System.out.println("Clicked Login and success");
                             Log.w(TAG, "signInWithCredential", task.getException());
                             Toast.makeText(Login.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            System.out.println("Clicked Login and failed");
                         }
                         // ...
                     }
@@ -416,4 +469,5 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
         Log.d(TAG, "onConnectionFailed:" + connectionResult);
         Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
     }
+
 }
